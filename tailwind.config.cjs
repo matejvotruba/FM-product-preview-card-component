7c9c7e39ea985c082573c484b47764bddb774615
matejvotruba <105660119+matejvotruba@users.noreply.github.com>
2023-03-31 18:08:19 +0200

    Accessibility changes Added hover effects

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,6 +11,7 @@ module.exports = {
     extend: {
       colors: {
         'dark-cyan': 'hsl(158, 36%, 37%)',
+        'darker-cyan': 'hsl(158, 36%, 18%)',
         'cream': 'hsl(30, 38%, 92%)',
         'very-dark-blue': 'hsl(212, 21%, 14%)',
         'dark-grayish-blue': 'hsl(228, 12%, 48%)',
